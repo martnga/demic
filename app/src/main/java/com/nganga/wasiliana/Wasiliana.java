42a5a7e65ada9ef43999abce1f6a3a67da8c24b1
mtnjoroge <mtnjoroge@gmail.com>
2015-08-02 19:18:59 +0300

    initialized Parse in Wasiliana Class

--- app/src/main/java/com/nganga/wasiliana/Wasiliana.java
+++ app/src/main/java/com/nganga/wasiliana/Wasiliana.java
@@ -2,6 +2,8 @@ package com.nganga.wasiliana;
 
 import android.app.Application;
 
+import com.parse.Parse;
+
 /**
  * Created by nganga on 8/2/15.
  */
@@ -10,5 +12,7 @@ public class Wasiliana extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Parse.initialize(this, "zxHHksqVd0dimMPxi4CIXoSJqJVBBQafUAqdjMjh", "ykERw4avQUCAHyD8d08Kaabyd75hGUOYiepDMSp0");
     }
 }
