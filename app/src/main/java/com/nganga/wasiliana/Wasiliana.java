e1a225b5c0b9efdd373eef9fe300e2ba8e27d424
mtnjoroge <mtnjoroge@gmail.com>
2015-08-02 19:13:51 +0300

    Wasiliana Class to hold the app data

---
+++ app/src/main/java/com/nganga/wasiliana/Wasiliana.java
@@ -0,0 +1,14 @@
+package com.nganga.wasiliana;
+
+import android.app.Application;
+
+/**
+ * Created by nganga on 8/2/15.
+ */
+public class Wasiliana extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
